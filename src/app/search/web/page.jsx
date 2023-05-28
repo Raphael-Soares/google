9f2821225a0e380b79034d37f8c7bf7bfc20fdfc
Raphael Soares <87400642+Raphael-Soares@users.noreply.github.com>
2023-05-28 18:52:12 +0000

    Fetch data from google search API

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,5 +1,14 @@
 import React from "react";
+const API_KEY = process.env.GOOGLE_API_KEY;
+const CONTEXT = process.env.GOOGLE_CONTEXT_KEY;
+export default async function page({ searchParams }) {
+    const response = await fetch(
+        `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT}&q=${searchParams.searchTerm} `
+    );
 
-export default function page() {
-    return <div>page</div>;
+    const data = await response.json();
+
+    const results = data.items;
+
+    return <div>{results && results.map((result) => <h1>{result.title}</h1>)}</div>;
 }
