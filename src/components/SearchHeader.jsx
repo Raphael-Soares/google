5d6acba153abc899db57103e7d2bb097b771ad8c
Raphael Soares <87400642+Raphael-Soares@users.noreply.github.com>
2023-05-28 01:31:25 +0000

    Update the search header component

--- src/components/SearchHeader.jsx
+++ src/components/SearchHeader.jsx
@@ -1,5 +1,38 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
+import SearchBox from "./SearchBox";
+import { RiSettings3Line } from "react-icons/ri";
+import { TbGridDots } from "react-icons/tb";
+import SearchHeaderOptions from "./SearchHeaderOptions";
 
 export default function SearchHeader() {
-    return <div>SearchHeader</div>;
+    return (
+        <header className="sticky top-0 w-full">
+            <div className="flex flex-row w-full p-6 items-center justify-between">
+                <Link href="">
+                    <Image
+                        width={120}
+                        height={40}
+                        src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/2560px-Google_2015_logo.svg.png"
+                    />
+                </Link>
+
+                <div className="flex-1">
+                    <SearchBox />
+                </div>
+
+                <div className="md:inline-flex hidden space-x-2 ">
+                    <RiSettings3Line className="header-icon" />
+                    <TbGridDots className="header-icon" />
+                </div>
+                <button className="ml-2 px-6 py-2 font-md rounded-md hover:brightness-105 hover:shadow-md transition-all bg-blue-500 text-white">
+                    Sign in
+                </button>
+            </div>
+            <div className="">
+                <SearchHeaderOptions />
+            </div>
+        </header>
+    );
 }
